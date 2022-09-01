9b27f7f92a9a798c347e9c1ed8e59558b4694919
EviSONG <evisong@live.com>
2022-09-01 13:02:35 +0800

    ch-05: 拆分看板和看板列组件

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const KanbanBoard = ({ children }) => (
+  <main className="kanban-board">{children}</main>
+);
+
+const KanbanColumn = ({ children, className }) => {
+  const combinedClassName = `kanban-column ${className}`;
+  return (
+    <section className={combinedClassName}>{children}</section>
+  );
+};
+
 const KanbanCard = ({ title, status }) => {
   return (
     <li className="kanban-card">
@@ -67,28 +78,28 @@ function App() {
         <h1>我的看板</h1>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <main className="kanban-board">
-        <section className="kanban-column column-todo">
+      <KanbanBoard>
+        <KanbanColumn className="column-todo">
           <h2>待处理<button onClick={handleAdd}
             disabled={showAdd}>&#8853; 添加新卡片</button></h2>
           <ul>
-            { showAdd && <KanbanNewCard onSubmit={handleSubmit} /> }
-            { todoList.map(props => <KanbanCard {...props} />) }
+          { showAdd && <KanbanNewCard onSubmit={handleSubmit} /> }
+          { todoList.map(props => <KanbanCard {...props} />) }
           </ul>
-        </section>
-        <section className="kanban-column column-ongoing">
+        </KanbanColumn>
+        <KanbanColumn className="column-ongoing">
           <h2>进行中</h2>
           <ul>
-            { ongoingList.map(props => <KanbanCard {...props} />) }
+          { ongoingList.map(props => <KanbanCard {...props} />) }
           </ul>
-        </section>
-        <section className="kanban-column column-done">
+        </KanbanColumn>
+        <KanbanColumn className="column-done">
           <h2>已完成</h2>
           <ul>
-            { doneList.map(props => <KanbanCard {...props} />) }
+          { doneList.map(props => <KanbanCard {...props} />) }
           </ul>
-        </section>
-      </main>
+        </KanbanColumn>
+      </KanbanBoard>
     </div>
   );
 }
