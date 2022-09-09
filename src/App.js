59d3cf9f359ac0bc6dfeb0b936dbc6608a8456d3
EviSONG <evisong@live.com>
2022-09-09 09:46:43 +0800

    ch-08: 卡片显示相对时间

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { css } from '@emotion/react';
 import logo from './logo.svg';
 import './App.css';
@@ -72,7 +72,33 @@ const kanbanCardTitleStyles = css`
   min-height: 3rem;
 `;
 
+const MINUTE = 60 * 1000;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+const UPDATE_INTERVAL = MINUTE;
 const KanbanCard = ({ title, status }) => {
+  const [displayTime, setDisplayTime] = useState(status);
+  useEffect(() => {
+    const updateDisplayTime = () => {
+      const timePassed = new Date() - new Date(status);
+      let relativeTime = '刚刚';
+      if (MINUTE <= timePassed && timePassed < HOUR) {
+        relativeTime = `${Math.ceil(timePassed / MINUTE)} 分钟前`;
+      } else if (HOUR <= timePassed && timePassed < DAY) {
+        relativeTime = `${Math.ceil(timePassed / HOUR)} 小时前`;
+      } else if (DAY <= timePassed) {
+        relativeTime = `${Math.ceil(timePassed / DAY)} 天前`;
+      }
+      setDisplayTime(relativeTime);
+    };
+    const intervalId = setInterval(updateDisplayTime, UPDATE_INTERVAL);
+    updateDisplayTime();
+
+    return function cleanup() {
+      clearInterval(intervalId);
+    };
+  }, [status]);
+
   return (
     <li css={kanbanCardStyles}>
       <div css={kanbanCardTitleStyles}>{title}</div>
@@ -80,7 +106,7 @@ const KanbanCard = ({ title, status }) => {
         text-align: right;
         font-size: 0.8rem;
         color: #333;
-      `}>{status}</div>
+      `} title={status}>{displayTime}</div>
     </li>
   );
 };
@@ -122,10 +148,10 @@ const COLUMN_BG_COLORS = {
 function App() {
   const [showAdd, setShowAdd] = useState(false);
   const [todoList, setTodoList] = useState([
-    { title: '开发任务-1', status: '22-05-22 18:15' },
-    { title: '开发任务-3', status: '22-05-22 18:15' },
-    { title: '开发任务-5', status: '22-05-22 18:15' },
-    { title: '测试任务-3', status: '22-05-22 18:15' }
+    { title: '开发任务-1', status: '2022-05-22 18:15' },
+    { title: '开发任务-3', status: '2022-06-22 18:15' },
+    { title: '开发任务-5', status: '2022-07-22 18:15' },
+    { title: '测试任务-3', status: '2022-07-23 18:15' }
   ]);
   const [ongoingList, setOngoingList ] = useState([
     { title: '开发任务-4', status: '2022-05-22 18:15' },
@@ -134,14 +160,14 @@ function App() {
   ]);
   const [doneList, setDoneList ] = useState([
     { title: '开发任务-2', status: '2022-06-24 18:15' },
-    { title: '测试任务-1', status: '2022-07-03 18:15' }
+    { title: '测试任务-1', status: '2022-07-03 18:15' }  
   ]);
   const handleAdd = (evt) => {
     setShowAdd(true);
   };
   const handleSubmit = (title) => {
     setTodoList(currentTodoList => [
-      { title, status: new Date().toDateString() },
+      { title, status: new Date().toString() },
       ...currentTodoList
     ]);
     setShowAdd(false);
